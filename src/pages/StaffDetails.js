5f701d72ebf1c60583c3616520cb96d069bda190
afikri <fikri.abdul@gmail.com>
2022-11-25 16:54:28 +0700

    Fix minor bug

--- src/pages/StaffDetails.js
+++ src/pages/StaffDetails.js
@@ -9,8 +9,8 @@ const StaffDetails = () => {
       <h1>Staff with id# - {id}</h1>
       <h3>
         <div key={id} >
-          <h2>Email: {data[0].email}</h2>
-          <p>Written by {data[0].staffname} </p>
+          <h2>Email: {data[id].email}</h2>
+          <p>Written by {data[id].staffname} </p>
         </div>
       </h3>
     </div>
